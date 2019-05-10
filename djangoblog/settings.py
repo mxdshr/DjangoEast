409523604cdeaa95691861c033ee67a92a58326d
reborn <reborn0502@163.com>
2019-05-10 20:05:06 +0800

    修改数据库

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -143,6 +143,10 @@ DATABASES = {
         'PASSWORD': '',
         'PORT': '3306',
         'HOST': 'localhost',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8',
+        },
     }
 }
 
