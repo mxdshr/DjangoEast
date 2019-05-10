a0a44f66af14e770e54c28345e8d9f7307cc8af1
reborn <reborn0502@163.com>
2019-05-10 22:03:46 +0800

    修改字段长度

--- setting/models.py
+++ setting/models.py
@@ -52,7 +52,7 @@ class Social(models.Model):
     wei_bo = models.URLField("微博地址", max_length=200, default='https://weibo.com/')
     zhi_hu = models.URLField("知乎地址", max_length=200, default='https://www.zhihu.com/people/sylax8/')
     qq = models.CharField("QQ号码", max_length=20, default='783342105')
-    wechat = models.CharField("微信",max_length=50,default='reborn0502')
+    wechat = models.CharField("微信",max_length=50, default='reborn0502')
     official_wechat = models.CharField("微信公众号", max_length=50, default='程序员向东')
 
     class Meta:
