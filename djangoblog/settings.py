de14f96562b0917add602b998053afb943cd3c6b
reborn <reborn0502@163.com>
2019-05-10 21:09:45 +0800

    查看版本

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -139,7 +139,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_east',
-        'USER': 'travis',
+        'USER': 'root',
         'PASSWORD': '',
         'PORT': '3306',
         'HOST': '127.0.0.1',
