da2bbcfba7c7c9483ce350ecfb5b22a572ae0f81
reborn <reborn0502@163.com>
2019-09-18 10:53:06 +0800

    asss

--- comment/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CommentConfig(AppConfig):
-    name = 'comment'
-
-    def ready(self):
-        super(CommentConfig,self).ready()
-        from . import signals
--- robot.py
+++
@@ -1,9 +0,0 @@
-#coding=utf8
-
-from werobot import WeRoBot
-robot = WeRoBot(token='weixin')
-@robot.handler
-def hello(message):
-    msg = message.content.strip().lower().encode('utf8')
-    if msg == 'python':
-        return '想学python啊'
